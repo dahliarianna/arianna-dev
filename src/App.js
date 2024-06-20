3e0dfc137586c89d012932a96c941eff3a2f300b
dahliarianna <dahliarianna@gmail.com>
2024-06-20 18:12:00 -0500

    updated timer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,4 @@
-// import "./App.css";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Home from "./Home";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <Router>
-//         <div className="App">
-//           <Routes>
-//             <Route path="/" element={<Home />} />
-//           </Routes>
-//         </div>
-//       </Router>
-//     </div>
-//   );
-// }
-
-// export default App;
 
 import React, { useState, useEffect } from "react";
 import "./App.css";
@@ -31,7 +13,7 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowClosedBook(false);
-    }, 1000);
+    }, 8000);
 
     return () => clearTimeout(timer);
   }, []);
